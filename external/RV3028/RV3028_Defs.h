2297fa9e62d57c56278c87a32be9d6e25337da5d
Daniel Kampert <danielkampert@kampis-elektroecke.de>
2020-12-14 21:59:49 +0100

    Update RV3028_Defs.h

--- external/RV3028/RV3028_Defs.h
+++ external/RV3028/RV3028_Defs.h
@@ -18,7 +18,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
-* Errors and commissions should be reported to DanielKampert@kampis-elektroecke.de
+* Errors and commissions should be reported to DanielKampert@kampis-elektroecke.de.
 *
 * <pre>
 * MODIFICATION HISTORY:
